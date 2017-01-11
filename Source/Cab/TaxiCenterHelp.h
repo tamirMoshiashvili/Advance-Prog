4b7a01d69dc47cb24a24275258dad5ee609ebd0f
Eden <edenshuker1997@gmail.com>
2017-01-12 00:51:51 +0200

    progress

--- Source/Cab/TaxiCenterHelp.h
+++ Source/Cab/TaxiCenterHelp.h
@@ -5,9 +5,20 @@
 #include <map>
 #include "../Socket/Tcp/TcpServer.h"
 #include "../Basic/Point.h"
+#include "Vehicle/Cab.h"
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <boost/archive/binary_iarchive.hpp>
 
 
-extern map<int, DriverInfo> g_driverIdToInfo;
+extern map<int, int> g_descriptorToDriverId;
+extern map<int, DriverInfo*> g_driverIdToInfo;
+
+using namespace std;
+using namespace boost;
 
 struct SocketInfo {
     TcpServer* serverSocket;
@@ -17,16 +28,21 @@ struct SocketInfo {
 struct DriverInfo{
     bool isAvailable;
     Point location;
-    int socketDescriptor;
     int cabId;
 };
 
 struct ConnectionInfo{
     pthread_t* pthread;
-    int socketDescriptor;
+    SocketInfo* socketInfo;
+};
+
+struct ParamToSendCab{
+    SocketInfo* socketInfo;
+    Cab* cab;
 };
 
 void* receiveDriverId(void* param);
+void* sendCab(void* param);
 
 
 #endif //EX2_TAXICENTERHELP_H
